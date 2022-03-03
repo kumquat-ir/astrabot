7a7be09a52674b1df5df81232c137079dd9b7e09
kumquat-ir <66188216+kumquat-ir@users.noreply.github.com>
2022-03-02 19:03:41 -0500

    tb-images, enable logging

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@
 import os
 import discord
 import json
+import logging
 
 from pathlib import Path
 
@@ -14,18 +15,23 @@ from textboxer import textboxer
 textboxer.resource_root = Path("tb-resources")
 helpdb = {
     "echo": "makes the bot say <text>",
-    "tb": textboxer.gen_help()
+    "tb": textboxer.gen_help(),
+    "tb_alias": """Searches the textbox image alias database to find what image names you can use for a given style.
+See "a> help tb" for a list of styles"""
 }
 usagedb = {
     "echo": "<text>",
-    "tb": "[style] [f:<flag>] <style options...>"
+    "tb": "[style] [f:<flag>] <style options...>",
+    "tb_alias": "<style> <search term>"
 }
 briefdb = {
     "echo": "make the bot say something",
-    "tb": "create a textbox image"
+    "tb": "create a textbox image",
+    "tb_alias": "what images you can use for textboxes"
 }
 
 
+logging.basicConfig(level=logging.INFO)
 intents = discord.Intents.default()
 intents.typing = False
 # intents.message_content = True
@@ -36,11 +42,20 @@ config_file.close()
 bot = commands.Bot(command_prefix="a> ", intents=intents)
 
 
-def command_with_help(*args, **kwargs):
+def command_with_help(_bot, *args, **kwargs):
 
     def decorator(func):
         fname = func.__name__
-        return bot.command(*args, **kwargs, help=helpdb[fname], usage=usagedb[fname], brief=briefdb[fname])(func)
+        return _bot.command(*args, **kwargs, help=helpdb[fname], usage=usagedb[fname], brief=briefdb[fname])(func)
+
+    return decorator
+
+
+def group_with_help(_bot, *args, **kwargs):
+
+    def decorator(func):
+        fname = func.__name__
+        return _bot.group(*args, **kwargs, help=helpdb[fname], usage=usagedb[fname], brief=briefdb[fname])(func)
 
     return decorator
 
@@ -52,13 +67,13 @@ async def require_admin(cxt: commands.Context) -> bool:
     return True
 
 
-@command_with_help()
+@command_with_help(bot)
 async def echo(cxt: commands.Context, *, arg: str):
     await cxt.send(arg)
 
 
-@bot.command(hidden=True)
-async def quit(cxt: commands.Context):
+@bot.command(hidden=True, name="quit")
+async def _quit(cxt: commands.Context):
     if await require_admin(cxt):
         await cxt.send("adios.")
         await bot.close()
@@ -85,13 +100,24 @@ async def _setstatus(text: str):
     await bot.change_presence(status=discord.Status.idle, activity=game)
 
 
-@command_with_help(aliases=["textbox"])
+@command_with_help(bot, aliases=["textbox"])
 async def tb(cxt: commands.Context, *args: str):
     textboxer.parsestr("", out="tb-tmp.png", presplit=list(args))
     tb_img = open("tb-tmp.png", "rb")
     await cxt.send(file=discord.File(tb_img, "generated textbox.png"))
     tb_img.close()
     os.remove("tb-tmp.png")
+
+
+@command_with_help(bot, name="tb-images", aliases=["tb-alias"])
+async def tb_alias(cxt: commands.Context, style: str, search: str):
+    alias_tmp = open("alias-tmp", "w+")
+    print(textboxer.find_aliases(style, search), file=alias_tmp)
+    alias_tmp.seek(0)
+    await cxt.send("alias search results for \"" + search + "\" in style " + style,
+                   file=discord.File(alias_tmp, "search-" + search + ".txt"))
+    alias_tmp.close()
+    os.remove("alias-tmp")
 
 
 @bot.event
